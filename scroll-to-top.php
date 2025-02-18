0926123055b67c2966b5b323ed7463689d0dec2b
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2025-02-18 10:38:21 +0600

    chnage script code's

--- scroll-to-top.php
+++ scroll-to-top.php
@@ -37,10 +37,18 @@ add_action('wp_enqueue_scripts', 'pstt_add_js_file');
 function pstt_activation()
 {
 ?>
-    <script>
-        jQuery(document).ready(function() {
-            $.scrollUp();
-        })
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-17303121-4']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
     </script>
 
 <?php
