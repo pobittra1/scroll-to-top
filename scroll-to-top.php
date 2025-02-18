b3e93279d488933204167b1618079c0a05f9edb6
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2025-02-18 11:29:12 +0600

    fix plugin scroll-to-top text issue. finaly done

--- scroll-to-top.php
+++ scroll-to-top.php
@@ -37,22 +37,22 @@ add_action('wp_enqueue_scripts', 'pstt_add_js_file');
 function pstt_activation()
 {
 ?>
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-17303121-4']);
-        _gaq.push(['_trackPageview']);
-        (function() {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
+    <script>
+        jQuery(document).ready(function() {
+            $.scrollUp();
+        })
+        jQuery(document).ready(function($) {
+            if ($.fn.scrollUp) {
+                $.scrollUp({
+                    scrollText: '',
+                    scrollSpeed: 500
+                });
+            } else {
+                console.error("ScrollUp plugin not loaded.");
+            }
+        });
     </script>
 
 <?php
 }
 add_action('wp_footer', 'pstt_activation');
-
-testfunction();
