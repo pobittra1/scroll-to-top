d2144c9dd94d7a554dfc316f43302f294a08c619
pobittra chandra roy <84267479+pobittra1@users.noreply.github.com>
2025-02-18 10:26:48 +0600

    again change

--- scroll-to-top.php
+++ scroll-to-top.php
@@ -16,75 +16,33 @@ Text Domain: pstt
 
 */
 
-// // include css and js file
-// // css
-// function pstt_add_css_file()
-// {
-//     wp_enqueue_style('pstt-style', plugins_url('css/pstt-style.css', __FILE__));
-// }
-// add_action('wp_enqueue_scripts', 'pstt_add_css_file');
+// include css and js file
+// css
+function pstt_add_css_file()
+{
+    wp_enqueue_style('pstt-style', plugins_url('css/pstt-style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'pstt_add_css_file');
 
-// //js
-// function pstt_add_js_file()
-// {
+//js
+function pstt_add_js_file()
+{
 
-//     wp_enqueue_script('jquery');
-//     wp_enqueue_script('pstt-js', plugins_url('js/pstt.pligin.js', __FILE__), array('jquery'), '1.0.0', true);
-// }
-// add_action('wp_enqueue_scripts', 'pstt_add_js_file');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('pstt-js', plugins_url('js/pstt.pligin.js', __FILE__), array('jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'pstt_add_js_file');
 
-// //plugin setting activation
-// function pstt_activation()
-// {
-// 
+//plugin setting activation
+function pstt_activation()
+{
 ?>
-// <script>
-    //         jQuery(document).ready(function() {
-    //             $.scrollUp();
-    //         })
-    //     
-</script>
-
-// <?php
-    // }
-    // add_action('wp_footer', 'pstt_activation');
-
-
-
-    // Enqueue CSS
-    function pstt_add_css_file()
-    {
-        wp_enqueue_style('pstt-style', plugin_dir_url(__FILE__) . 'css/pstt-style.css');
-    }
-    add_action('wp_enqueue_scripts', 'pstt_add_css_file');
-
-    // Enqueue JS
-    function pstt_add_js_file()
-    {
-        wp_enqueue_script('jquery'); // Ensure jQuery is loaded
-        wp_enqueue_script(
-            'pstt-js',
-            plugin_dir_url(__FILE__) . 'js/pstt.plugin.js',  // Correct the filename if it's a typo
-            array('jquery'),
-            '1.0.0',
-            true // Load in the footer
-        );
-    }
-    add_action('wp_enqueue_scripts', 'pstt_add_js_file');
-
-    // Plugin Activation
-    function pstt_activation()
-    {
-    ?>
     <script>
-        jQuery(document).ready(function($) {
-            if ($.fn.scrollUp) {
-                $.scrollUp();
-            } else {
-                console.error("ScrollUp plugin not loaded.");
-            }
-        });
+        jQuery(document).ready(function() {
+            $.scrollUp();
+        })
     </script>
+
 <?php
-    }
-    add_action('wp_footer', 'pstt_activation');
+}
+add_action('wp_footer', 'pstt_activation');
